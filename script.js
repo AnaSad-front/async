ce7a0bb3e6dd008cd7b1381f8d0b07dd131b9768
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-11-21 16:02:33 +0300

    running promises in parallel

--- script.js
+++ script.js
@@ -39,6 +39,14 @@ const renderError = function (msg) {
   countriesContainer.style.opacity = 1;
 };
 
+const getJSON = function (url, errorMsg = "Something went wrong") {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
 /*
 const getCountryAndNeighbour = function (country) {
   // AJAX call country 1
@@ -227,7 +235,7 @@ btn.addEventListener("click", whereAmI);
 */
 
 // CONSUMING PROMISES WITH ASYNC AWAIT
-
+/*
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -250,7 +258,7 @@ const whereAmI = async function () {
 
     // Country data
     const res = await fetch(
-      `https://restcountries.com/v3.1/name/${dataGeo.countryCode}`
+      `https://restcountries.com/v3.1/name/${dataGeo.countryName}`
     );
     if (!res.ok) throw new Error("Problem getting country");
     const data = await res.json();
@@ -283,3 +291,23 @@ console.log("Will get location");
   }
   console.log("3: Finished getting location");
 })();
+*/
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all(
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`)
+    );
+    console.log(data.map((d) => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+get3Countries("portugal", "canada", "tanzania");
